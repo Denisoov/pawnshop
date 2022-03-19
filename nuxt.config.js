f9a675ea58b7199544124bff25a4308bf0c1be13
Denisoov <denisoov07@gmail..com>
2022-03-19 19:41:48 +0700

    настроил структуру стилей

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,8 +19,14 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/money.ico' }
     ]
   },
+  styleResources: {
+    scss: [
+    '@/assets/styles/colors.scss',
+    '@/assets/styles/fonts.scss',
+    ],
+  },
   css: [
-  ],
+    '@/assets/styles/index.scss'],
   plugins: [
   ],
   components: true,
@@ -28,23 +34,11 @@ export default {
     '@nuxtjs/vuetify',
   ],
   modules: [
+    '@nuxtjs/style-resources'
   ],
   vuetify: {
-    customVariables: ['~/assets/variables.scss'],
-    theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
-    }
+    treeShake: true,
+    defaultAssets: false,
   },
   build: {
   }
