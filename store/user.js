8df8d6b630b645bf3908b1c872e53d4d61deb2c7
Denisoov <denisoov07@gmail..com>
2022-03-20 01:15:52 +0700

    базовые поля для пользователя

--- store/user.js
+++ store/user.js
@@ -1,8 +1,25 @@
 export const state = () => ({
+    firstName: null,
+    secondName: null,
+    patronymic: null,
+    phone: null,
+    passport: {
+        seria: null,
+        number: null,
+    }
+
 })
+
 export const actions = {
 }
+
 export const mutations = {
 }
+
 export const getters = {
+    userName: ({ firstName, patronymic }) => {
+        const namePatronymic = `${firstName} ${patronymic[0]}`
+
+        return namePatronymic
+    }
 }
