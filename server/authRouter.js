fb4a1da885fcc19ea8b7182435e4fb2e76865644
Denisoov <denisoov07@gmail..com>
2022-03-23 15:30:32 +0700

    Авторизация для пользователя

--- server/authRouter.js
+++ server/authRouter.js
@@ -8,6 +8,6 @@ router.post('/registration', [
     check('username', "Имя пользователя не может быть пустым").notEmpty(),
     check('password', "Пароль должен быть больше 4 символов").isLength({ min: 4 })
 ], authController.registration)
-router.get('/login', authController.login)
+router.post('/login', authController.login)
 
 export default router
--- server/controls/authController.js
+++ server/controls/authController.js
@@ -2,20 +2,28 @@ import User from '../models/User.js'
 import Role from '../models/Role.js'
 
 import bcrypt from 'bcryptjs'
-
+import jwt from 'jsonwebtoken'
 import { validationResult } from 'express-validator'
 
+const generateAccessToken = (id, role) => {
+    const payload = {
+        id,
+        role
+    }
+    
+    return jwt.sign(payload, "SECRET_KEY")
+}
 class AuthController {
     async registration(req, res) {
         try {
             const errors = validationResult(req)
 
-            if (!errors.isEmpty()) return res.status(400).json('Ошибка при регистрации', errors)
-            
+            if (!errors.isEmpty()) return res.status(400).json({ error: 'Заполните все поля' })
+
             const { username, password } = req.body
             const candidate = await User.findOne({ username })
 
-            if (candidate) return res.status(400).json('Пользователь с таким логином уже существует')
+            if (candidate) return res.status(400).json({ error: 'Пользователь с таким логином уже существует' })
             
             const hashPassword = bcrypt.hashSync(password, 7);
 
@@ -29,15 +37,28 @@ class AuthController {
 
             user.save()
 
-            return res.json('Вы успешло зарегестрировали пользователя')
+            return res.json({ error: 'Вы успешло зарегестрировали пользователя' })
 
         } catch (error) {
-            res.status(400).json('Невозможно зарегестрироваться')
+            res.status(400).json({ error: 'Невозможно зарегестрироваться' })
         }
     }
     async login(req, res) {
         try {
-            console.log('hello')
+            const { username, password } = req.body
+            const candidate = await User.findOne({ username })
+            
+            if (!candidate) return res.status(400).json({ error: `Пользователь ${username} не найден` })
+
+            const validPassword = bcrypt.compareSync(password, candidate.password)
+
+            if (!validPassword) return res.status(400).json({ error: `Пароль неверный` })
+
+            const token = generateAccessToken(candidate._id, candidate.role)
+
+            return res.json({ token })
+
+
         } catch (error) {
             console.log(error)
             res.status(400).json('Невозможно авторизоваться')
