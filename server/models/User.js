25911a6263f48bec78b3eef8927b3f32962fcd8a
Denisoov <denisoov07@gmail..com>
2022-03-24 17:34:11 +0700

    добавил в модель поле создания пользотвалея

--- server/models/User.js
+++ server/models/User.js
@@ -4,7 +4,8 @@ const { Schema, model } = pkg;
 const User = new Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true },
-    role: { type: String, ref: 'Role' }
-})
+    role: { type: String, ref: 'Role' },
+},
+{ timestamps: true })
 
 export default model('User', User)
